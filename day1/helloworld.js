87da991840e81d5c540aefd7897a78b817b13846
scezv <susanshrestha783@gmail.com>
2022-12-20 23:33:41 +0545

    js fundamentals

---
+++ day1/helloworld.js
@@ -0,0 +1,13 @@
+console.log('Hello world!!!')
+jss = 'amazing'
+if (jss === 'amazing') alert("JSJS")
+console.log(40 + 566 + 69)
+
+firstName = "Serbes"
+secondName = "Sary"
+
+console.log(firstName + ' ' + secondName)
+
+PI = 22 / 7
+
+console.log(PI)
---
+++ day1/index.js
@@ -0,0 +1,10 @@
+console.log("This is an external script alert")
+// Declaring different variables of different data types
+let firstName = 'Asabeneh' // first name of a person
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age = 100 // age in years
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age, isMarried)
